9d687988137c795a49fe43b7cbb830b944e33c24
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-09-11 19:16:07 +0900

    スペルの修正

--- src/App.jsx
+++ src/App.jsx
@@ -48,7 +48,7 @@ export const App = () => {
         todoText={todoText}
         onChange={onChangeTodoText}
         onClick={onClickAdd}
-        disabeld={incompleteTodos.length >= 5}
+        disabled={incompleteTodos.length >= 5}
       />
       {incompleteTodos.length >= 5 && (
         <p style={{ color: "red" }}>
