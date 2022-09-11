1a17503e775634b8c242da5e1aecd0db36d9690c
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-09-11 17:51:39 +0900

    IncompleteTodosコンポーネントの作成と未完了TODO部分切り出し

--- src/App.jsx
+++ src/App.jsx
@@ -47,20 +47,6 @@ export const App = () => {
         onChange={onChangeTodoText}
         onClick={onClickAdd}
       />
-      <div className="incomplete-area">
-        <p className="title">未完了のTODO</p>
-        <ul>
-          {incompleteTodos.map((todo, index) => {
-            return (
-              <div key={todo} className="list-row">
-                <li>{todo}</li>
-                <button onClick={() => onClickComplete(index)}>完了</button>
-                <button onClick={() => onClickDelete(index)}>削除</button>
-              </div>
-            );
-          })}
-        </ul>
-      </div>
       <div className="complete-area">
         <p className="title">完了のTODO</p>
         <ul>
